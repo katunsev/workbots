20d9d53acbb187bdb90b33e16a7c1df982d7e925
d.katunsev <d.katunsev.gc@gmail.com>
2025-05-20 16:59:01 +1000

    cron

--- public/cron.sh
+++ public/cron.sh
@@ -9,7 +9,7 @@ add_cron_job() {
     grep -F "$job" "$CRON_TEMP" >/dev/null 2>&1 || echo "$job" >> "$CRON_TEMP"
 }
 
-add_cron_job "* * * * * /usr/bin/php /var/www/katunsev.ru/public/cron.php >> /var/log/cron.php.log 2>&1"
+add_cron_job "*/30 9-18 * * 1-5 /usr/bin/php /var/www/katunsev.ru/public/cron.php >> /var/log/cron.php.log 2>&1"
 #add_cron_job "0 1 * * * /usr/bin/php /var/www/katunsev.ru/public/create-tickets.php >> /var/log/create-tickets.log 2>&1"
 #add_cron_job "30 2 * * * /usr/bin/php /var/www/katunsev.ru/public/another-script.php >> /var/log/another-script.log 2>&1"
 
